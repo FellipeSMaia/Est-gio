f351cb9e11bb3e0132687bac64bf6a9bc3c68d7f
FellipeSMaia <fellipesouzamaia1@gmail.com>
2024-05-04 18:07:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function adicionarTarefa() {
+    const valorInput = document.getElementById("novaTarefa").value
+
+    if (valorInput !== "") {
+        console.log("teste")
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+        const del = document.createElement("button")
+        const inputCheck = document.createElement("input")
+        const label = document.createElement("label")
+
+
+        const listatarefas = document.getElementById("listaTarefas")
+
+        div.className = "tarefas"
+        p.className = "p"
+        del.className = "del"
+        inputCheck.className = "check"
+        inputCheck.setAttribute("type", "checkbox")
+
+        p.innerHTML = valorInput
+        del.innerText = "Deletar Tarefa"
+
+        label.append(inputCheck)
+        const div2 = document.createElement("div")
+        div2.appendChild(label)
+        div2.appendChild(del)
+
+
+        div.appendChild(p)
+        div.appendChild(div2)
+
+        listatarefas.append(div)
+
+        del.onclick = () => {
+            div.remove()
+        }
+
+        inputCheck.addEventListener('change', function () {
+            if (this.checked) {
+                p.style.textDecoration = "line-through";
+
+                div.style.backgroundColor = "green"
+            } else {
+                p.style.textDecoration = "none";
+
+            }
+        });
+
+
+        document.getElementById("novaTarefa").value = "";
+    } else {
+        alert("Por favor, insira uma tarefa antes de adicionar.");
+    }
+
+}
